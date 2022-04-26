6d9983a8fe939ce411531410394d685e2329ae0a
Saran-73 <saransamikannu@gmail.com>
2022-04-27 00:19:40 +0530

    m

--- src/index.js
+++ src/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App';
+const container=document.getElementById('root')
+const root=createRoot(container)
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
-// ReactDOM.createRoot(document.getElementById('root')).render(<App/>)
+ root.render(<App/>)
